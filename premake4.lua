fa4ab80e3e05dac879c03f185f2e50dab9f3d920
Julien Hamaide <julien.hamaide@fishingcactus.com>
2014-03-23 17:48:35 +0100

    premake file

---
+++ premake4.lua
@@ -0,0 +1,75 @@
+newoption
+{
+    trigger = "ios",
+    description = "generates ios project without native target"
+}
+
+solution "ShaderShaker"
+    configurations { "Release", "Debug" }
+
+    targetname  "shader_shaker"
+    language    "C++"
+    flags       { "ExtraWarnings" }
+    includedirs { "include", "contrib", "src/hlsl_parser" }
+
+    files
+    {
+        "src/**.h", "src/**.cpp"
+    }
+
+    if _OPTIONS[ "ios" ] then
+        platforms { "ios" }
+
+        if deploymenttarget then
+            deploymenttarget "5.1"
+        end
+
+    end
+
+    configuration "Debug"
+        defines     { "_DEBUG" }
+        flags       { "Symbols" }
+
+    configuration "Release"
+        defines     { "NDEBUG" }
+        flags       { "OptimizeSize" }
+
+    configuration "vs*"
+        defines     { "_CRT_SECURE_NO_WARNINGS" }
+
+project "ShaderShaker"
+
+    kind        "ConsoleApp"
+
+    configuration "Debug"
+        targetdir   "bin/debug"
+
+    configuration "Release"
+        targetdir   "bin/release"
+
+project "ShaderShakerDll"
+    defines{ "SHADERSHAKER_IN_DLL" }
+
+    kind "SharedLib"
+
+    configuration "Debug"
+        targetdir   "bin/debug_dll"
+
+    configuration "Release"
+        targetdir   "bin/release_dll"
+
+project "ShaderShakerLib"
+
+    defines{ "SHADERSHAKER_IN_LIB" }
+    kind "StaticLib"
+
+    configuration "Debug"
+        targetdir   "bin/debug_lib"
+
+    configuration "Release"
+        targetdir   "bin/release_lib"
+
+if _ACTION == "clean" then
+    os.rmdir("bin")
+    os.rmdir("build")
+end
