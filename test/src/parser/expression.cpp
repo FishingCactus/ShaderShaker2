352f76813c15f84f27691c63ae04c876bc8b043b
Julien Hamaide <julien.hamaide@fishingcactus.com>
2014-04-02 16:47:24 +0200

    Literal parsing test

--- test/src/parser/expression.cpp
+++ test/src/parser/expression.cpp
@@ -3,6 +3,113 @@
 #include "parser_helper.h"
 
 
+AST::Expression * ParseLiteralExpression( const char * code )
+{
+    Parser parser( code, strlen( code ) );
+
+    return  parser.m_Parser.literal_value();
+}
+
+TEST_CASE( "Literal are parsed", "[parser]" )
+{
+    AST::Expression * expression = 0;
+    AST::LiteralExpression * literal_expression;
+
+    SECTION( "Integer are parsed" )
+    {
+        expression = ParseLiteralExpression( " 1234 " );
+
+        REQUIRE( expression );
+
+        literal_expression = dynamic_cast<AST::LiteralExpression*>( expression );
+
+        REQUIRE( literal_expression );
+        CHECK( literal_expression->m_Type == AST::LiteralExpression::Int );
+        CHECK( literal_expression->m_Value == "1234" );
+
+    }
+
+    SECTION( "Float are parsed" )
+    {
+        expression = ParseLiteralExpression( " 1234.5f " );
+
+        REQUIRE( expression );
+
+        literal_expression = dynamic_cast<AST::LiteralExpression*>( expression );
+
+        REQUIRE( literal_expression );
+        CHECK( literal_expression->m_Type == AST::LiteralExpression::Float );
+        CHECK( literal_expression->m_Value == "1234.5f" );
+    }
+
+    SECTION( "Float are parsed2" )
+    {
+        expression = ParseLiteralExpression( " 1234.0 " );
+
+        REQUIRE( expression );
+
+        literal_expression = dynamic_cast<AST::LiteralExpression*>( expression );
+
+        REQUIRE( literal_expression );
+        CHECK( literal_expression->m_Type == AST::LiteralExpression::Float );
+        CHECK( literal_expression->m_Value == "1234.0" );
+    }
+
+    SECTION( "Float are parsed3" )
+    {
+        expression = ParseLiteralExpression( " .123f " );
+
+        REQUIRE( expression );
+
+        literal_expression = dynamic_cast<AST::LiteralExpression*>( expression );
+
+        REQUIRE( literal_expression );
+        CHECK( literal_expression->m_Type == AST::LiteralExpression::Float );
+        CHECK( literal_expression->m_Value == ".123f" );
+    }
+
+    SECTION( "Float are parsed4" )
+    {
+        expression = ParseLiteralExpression( " .123 " );
+
+        REQUIRE( expression );
+
+        literal_expression = dynamic_cast<AST::LiteralExpression*>( expression );
+
+        REQUIRE( literal_expression );
+        CHECK( literal_expression->m_Type == AST::LiteralExpression::Float );
+        CHECK( literal_expression->m_Value == ".123" );
+    }
+
+    SECTION( "True is parsed" )
+    {
+        expression = ParseLiteralExpression( " true " );
+
+        REQUIRE( expression );
+
+        literal_expression = dynamic_cast<AST::LiteralExpression*>( expression );
+
+        REQUIRE( literal_expression );
+        CHECK( literal_expression->m_Type == AST::LiteralExpression::Bool );
+        CHECK( literal_expression->m_Value == "true" );
+    }
+
+    SECTION( "False is parsed" )
+    {
+        expression = ParseLiteralExpression( " false " );
+
+        REQUIRE( expression );
+
+        literal_expression = dynamic_cast<AST::LiteralExpression*>( expression );
+
+        REQUIRE( literal_expression );
+        CHECK( literal_expression->m_Type == AST::LiteralExpression::Bool );
+        CHECK( literal_expression->m_Value == "false" );
+    }
+
+    delete expression;
+}
+
 TEST_CASE( "Arithmetic expression are parsed", "[parser]" )
 {
     AST::Expression * expression = 0;
