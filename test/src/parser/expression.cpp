2475cfcc120224e92e49f97f90e7a368db3500b2
Julien Hamaide <julien.hamaide@fishingcactus.com>
2014-04-03 08:01:17 +0200

    Extract parser helper in it's own class

--- test/src/parser/expression.cpp
+++ test/src/parser/expression.cpp
@@ -1,27 +1,7 @@
 #include "catch.hpp"
-#include "hlsl_parser/HLSLLexer.hpp"
-#include "hlsl_parser/HLSLParser.hpp"
 #include "ast/node.h"
+#include "parser_helper.h"
 
-struct Parser
-{
-    Parser( const char * code, const int size ) :
-        m_Input( (ANTLR_UINT8* )code, ANTLR_ENC_8BIT, size, (ANTLR_UINT8*) "" ),
-        m_Lexer( &m_Input ),
-        m_TokenStream( ANTLR_SIZE_HINT, m_Lexer.get_tokSource() ),
-        m_Parser( &m_TokenStream )
-    {
-    }
-
-    HLSLLexerTraits::InputStreamType
-        m_Input;
-    HLSLLexer
-        m_Lexer;
-    HLSLLexerTraits::TokenStreamType
-        m_TokenStream;
-    HLSLParser
-        m_Parser;
-};
 
 TEST_CASE( "Arithmetic expression are parsed", "[parser]" )
 {
---
+++ test/src/parser/parser_helper.h
@@ -0,0 +1,27 @@
+#ifndef PARSER_HELPER_H
+    #define PARSER_HELPER_H
+
+    #include "hlsl_parser/HLSLLexer.hpp"
+    #include "hlsl_parser/HLSLParser.hpp"
+
+    struct Parser
+    {
+        Parser( const char * code, const int size ) :
+            m_Input( (ANTLR_UINT8* )code, ANTLR_ENC_8BIT, size, (ANTLR_UINT8*) "" ),
+            m_Lexer( &m_Input ),
+            m_TokenStream( ANTLR_SIZE_HINT, m_Lexer.get_tokSource() ),
+            m_Parser( &m_TokenStream )
+        {
+        }
+
+    HLSLLexerTraits::InputStreamType
+        m_Input;
+    HLSLLexer
+        m_Lexer;
+    HLSLLexerTraits::TokenStreamType
+        m_TokenStream;
+    HLSLParser
+        m_Parser;
+    };
+
+#endif
