b9af8d883dda3a75952fc2e45f7d005da47132cc
Julien Hamaide <julien.hamaide@fishingcactus.com>
2014-04-21 00:49:15 +0200

    Start of hlsl printer from ast

---
+++ src/ast/printer/hlsl_printer.cpp
@@ -0,0 +1,83 @@
+#include "hlsl_printer.h"
+
+
+namespace AST
+{
+
+    void HLSLPrinter::Visit( Node & node )
+    {
+        assert( "Unsupported node type, implement in base class" );
+    }
+
+    void HLSLPrinter::Visit( TranslationUnit & translation_unit )
+    {
+
+
+    }
+
+    void HLSLPrinter::Visit( VariableDeclaration & variable_declaration )
+    {
+
+
+    }
+
+    void HLSLPrinter::Visit( IntrinsicType & type )
+    {
+
+
+    }
+
+    void HLSLPrinter::Visit( UserDefinedType & type )
+    {
+
+
+    }
+
+    void HLSLPrinter::Visit( SamplerType & type )
+    {
+
+
+    }
+
+    void HLSLPrinter::Visit( TypeModifier & modifier )
+    {
+
+
+    }
+
+    void HLSLPrinter::Visit( StorageClass & storage_class )
+    {
+
+
+    }
+
+    void HLSLPrinter::Visit( VariableDeclarationBody & body )
+    {
+
+
+    }
+
+    void HLSLPrinter::Visit( TextureDeclaration & declaration )
+    {
+
+
+    }
+
+    void HLSLPrinter::Visit( SamplerDeclaration & declaration )
+    {
+
+
+    }
+
+    void HLSLPrinter::Visit( SamplerBody & body )
+    {
+
+
+    }
+
+    void HLSLPrinter::Visit( StructDefinition & definition )
+    {
+
+
+    }
+}
---
+++ src/ast/printer/hlsl_printer.h
@@ -0,0 +1,40 @@
+#ifndef HLSL_PRINTER
+    #define HLSL_PRINTER
+
+    #include "ast/visitor.h"
+    #include <ostream>
+
+    namespace AST
+    {
+
+        class HLSLPrinter : public Visitor
+        {
+        public:
+
+            HLSLPrinter( std::ostream & stream ) : m_Stream( stream ){}
+
+            virtual void Visit( Node & node ) override;
+            virtual void Visit( TranslationUnit & translation_unit ) override;
+            virtual void Visit( VariableDeclaration & variable_declaration ) override;
+            virtual void Visit( IntrinsicType & type ) override;
+            virtual void Visit( UserDefinedType & type ) override;
+            virtual void Visit( SamplerType & type ) override;
+            virtual void Visit( TypeModifier & modifier ) override;
+            virtual void Visit( StorageClass & storage_class ) override;
+            virtual void Visit( VariableDeclarationBody & body ) override;
+            virtual void Visit( TextureDeclaration & declaration ) override;
+            virtual void Visit( SamplerDeclaration & declaration ) override;
+            virtual void Visit( SamplerBody & body ) override;
+            virtual void Visit( StructDefinition & definition ) override;
+
+
+        private:
+
+            std::ostream
+                & m_Stream;
+
+        };
+    }
+
+
+#endif
