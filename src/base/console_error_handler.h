6ebe5827df3504e2bfc5f76476c314807d0c82c7
Julien Hamaide <julien.hamaide@fishingcactus.com>
2015-03-11 20:35:08 +0100

    Convert some tabs into spaces

--- src/base/console_error_handler.h
+++ src/base/console_error_handler.h
@@ -1,18 +1,16 @@
-#ifndef CONSOLE_ERROR_HANDLER_H
-	#define CONSOLE_ERROR_HANDLER_H
+#pragma once
 
-	#include <base/error_handler_interface.h>
+#include <base/error_handler_interface.h>
 
-	namespace Base
-	{
-		class ConsoleErrorHandler : public ErrorHandlerInterface
-		{
-		public:
+namespace Base
+{
+    class ConsoleErrorHandler : public ErrorHandlerInterface
+    {
+    public:
 
-			void ReportError(
-				const std::string & message,
-				const std::string & file
-				) override;
-		};
-	}
-#endif
+        void ReportError(
+            const std::string & message,
+            const std::string & file
+            ) override;
+    };
+}
--- src/base/error_handler_interface.h
+++ src/base/error_handler_interface.h
@@ -1,29 +1,26 @@
-#ifndef ERROR_HANDLER_INTERFACE_H
-	#define ERROR_HANDLER_INTERFACE_H
+#pragma once
 
-    #include <base/object.h>
-    #include <base/object_ref.h>
-    #include <string>
+#include <base/object.h>
+#include <base/object_ref.h>
+#include <string>
 
-	namespace Base
-	{
-		class ErrorHandlerInterface : public Object
-		{
-		public:
+namespace Base
+{
+    class ErrorHandlerInterface : public Object
+    {
+    public:
 
-            typedef ObjectRef<ErrorHandlerInterface>
-                Ref;
+        typedef ObjectRef<ErrorHandlerInterface>
+            Ref;
 
-			virtual ~ErrorHandlerInterface()
-			{
+        virtual ~ErrorHandlerInterface()
+        {
 
-			}
+        }
 
-			virtual void ReportError(
-				const std::string & message,
-				const std::string & file
-				) = 0;
-		};
-	}
-
-#endif
+        virtual void ReportError(
+            const std::string & message,
+            const std::string & file
+            ) = 0;
+    };
+}
