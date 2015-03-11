e6f60e2340fd0749d06588b905d5c4b2f16af575
Michael Delva <michael.delva@fishingcactus.com>
2015-03-11 16:32:57 +0100

    Put forward declarations in visitor_base in function arguments

--- src/ast/visitor_base.h
+++ src/ast/visitor_base.h
@@ -4,57 +4,6 @@
 
 namespace AST
 {
-    struct Node;
-    struct TranslationUnit;
-    struct GlobalDeclaration;
-    struct VariableDeclaration;
-    struct Type;
-    struct IntrinsicType;
-    struct UserDefinedType;
-    struct SamplerType;
-    struct Technique;
-    struct TypeModifier;
-    struct StorageClass;
-    struct VariableDeclarationBody;
-    struct TextureDeclaration;
-    struct SamplerDeclaration;
-    struct SamplerBody;
-    struct StructDefinition;
-    struct FunctionDeclaration;
-    struct ArgumentList;
-    struct Argument;
-
-    struct LiteralExpression;
-    struct VariableExpression;
-    struct UnaryOperationExpression;
-    struct BinaryOperationExpression;
-    struct CallExpression;
-    struct ArgumentExpressionList;
-    struct Swizzle;
-    struct PostfixSuffixCall;
-    struct PostfixSuffixVariable;
-    struct ConstructorExpression;
-    struct ConditionalExpression;
-    struct LValueExpression;
-    struct PreModifyExpression;
-    struct PostModifyExpression;
-    struct CastExpression;
-    struct AssignmentExpression;
-    struct PostfixExpression;
-
-    struct ReturnStatement;
-    struct BreakStatement;
-    struct ContinueStatement;
-    struct DiscardStatement;
-    struct EmptyStatement;
-    struct ExpressionStatement;
-    struct IfStatement;
-    struct WhileStatement;
-    struct DoWhileStatement;
-    struct BlockStatement;
-    struct AssignmentStatement;
-    struct VariableDeclarationStatement;
-
     template<bool is_const>
     class VisitorBase
     {
@@ -63,56 +12,56 @@ namespace AST
         using modifier = typename std::conditional<is_const, add_const, identity>::type;
 
     public:
-        virtual void Visit( typename modifier::template m<Node>::type & node ) = 0;
-        virtual void Visit( typename modifier::template m<TranslationUnit>::type & translation_unit ) = 0;
-        virtual void Visit( typename modifier::template m<VariableDeclaration>::type & variable_declaration ) = 0;
-        virtual void Visit( typename modifier::template m<IntrinsicType>::type & type ) = 0;
-        virtual void Visit( typename modifier::template m<UserDefinedType>::type & type ) = 0;
-        virtual void Visit( typename modifier::template m<SamplerType>::type & type ) = 0;
-        virtual void Visit( typename modifier::template m<TypeModifier>::type & modifier ) = 0;
-        virtual void Visit( typename modifier::template m<StorageClass>::type & storage_class ) = 0;
-        virtual void Visit( typename modifier::template m<VariableDeclarationBody>::type & body ) = 0;
-        virtual void Visit( typename modifier::template m<TextureDeclaration>::type & declaration ) = 0;
-        virtual void Visit( typename modifier::template m<SamplerDeclaration>::type & declaration ) = 0;
-        virtual void Visit( typename modifier::template m<SamplerBody>::type & body ) = 0;
-        virtual void Visit( typename modifier::template m<StructDefinition>::type & definition ) = 0;
-        virtual void Visit( typename modifier::template m<FunctionDeclaration>::type & function_declaration ) = 0;
-        virtual void Visit( typename modifier::template m<ArgumentList>::type & argument_list ) = 0;
-        virtual void Visit( typename modifier::template m<Argument>::type & argument ) = 0;
+        virtual void Visit( typename modifier::template m< struct Node>::type & node ) = 0;
+        virtual void Visit( typename modifier::template m< struct TranslationUnit>::type & translation_unit ) = 0;
+        virtual void Visit( typename modifier::template m< struct VariableDeclaration>::type & variable_declaration ) = 0;
+        virtual void Visit( typename modifier::template m< struct IntrinsicType>::type & type ) = 0;
+        virtual void Visit( typename modifier::template m< struct UserDefinedType>::type & type ) = 0;
+        virtual void Visit( typename modifier::template m< struct SamplerType>::type & type ) = 0;
+        virtual void Visit( typename modifier::template m< struct TypeModifier>::type & modifier ) = 0;
+        virtual void Visit( typename modifier::template m< struct StorageClass>::type & storage_class ) = 0;
+        virtual void Visit( typename modifier::template m< struct VariableDeclarationBody>::type & body ) = 0;
+        virtual void Visit( typename modifier::template m< struct TextureDeclaration>::type & declaration ) = 0;
+        virtual void Visit( typename modifier::template m< struct SamplerDeclaration>::type & declaration ) = 0;
+        virtual void Visit( typename modifier::template m< struct SamplerBody>::type & body ) = 0;
+        virtual void Visit( typename modifier::template m< struct StructDefinition>::type & definition ) = 0;
+        virtual void Visit( typename modifier::template m< struct FunctionDeclaration>::type & function_declaration ) = 0;
+        virtual void Visit( typename modifier::template m< struct ArgumentList>::type & argument_list ) = 0;
+        virtual void Visit( typename modifier::template m< struct Argument>::type & argument ) = 0;
 
         // Expressions
-        virtual void Visit( typename modifier::template m<LiteralExpression>::type & expression ) = 0;
-        virtual void Visit( typename modifier::template m<VariableExpression>::type & expression ) = 0;
-        virtual void Visit( typename modifier::template m<UnaryOperationExpression>::type & expression ) = 0;
-        virtual void Visit( typename modifier::template m<BinaryOperationExpression>::type & expression ) = 0;
-        virtual void Visit( typename modifier::template m<CallExpression>::type & expression ) = 0;
-        virtual void Visit( typename modifier::template m<ArgumentExpressionList>::type & list ) = 0;
-        virtual void Visit( typename modifier::template m<Swizzle>::type & swizzle ) = 0;
-        virtual void Visit( typename modifier::template m<PostfixSuffixCall>::type & postfix_suffix ) = 0;
-        virtual void Visit( typename modifier::template m<PostfixSuffixVariable>::type & postfix_suffix ) = 0;
-        virtual void Visit( typename modifier::template m<ConstructorExpression>::type & expression ) = 0;
-        virtual void Visit( typename modifier::template m<ConditionalExpression>::type & expression ) = 0;
-        virtual void Visit( typename modifier::template m<LValueExpression>::type & expression ) = 0;
-        virtual void Visit( typename modifier::template m<PreModifyExpression>::type & expression ) = 0;
-        virtual void Visit( typename modifier::template m<PostModifyExpression>::type & expression ) = 0;
-        virtual void Visit( typename modifier::template m<CastExpression>::type & expression ) = 0;
-        virtual void Visit( typename modifier::template m<AssignmentExpression>::type & expression ) = 0;
-        virtual void Visit( typename modifier::template m<PostfixExpression>::type & expression ) = 0;
+        virtual void Visit( typename modifier::template m< struct LiteralExpression>::type & expression ) = 0;
+        virtual void Visit( typename modifier::template m< struct VariableExpression>::type & expression ) = 0;
+        virtual void Visit( typename modifier::template m< struct UnaryOperationExpression>::type & expression ) = 0;
+        virtual void Visit( typename modifier::template m< struct BinaryOperationExpression>::type & expression ) = 0;
+        virtual void Visit( typename modifier::template m< struct CallExpression>::type & expression ) = 0;
+        virtual void Visit( typename modifier::template m< struct ArgumentExpressionList>::type & list ) = 0;
+        virtual void Visit( typename modifier::template m< struct Swizzle>::type & swizzle ) = 0;
+        virtual void Visit( typename modifier::template m< struct PostfixSuffixCall>::type & postfix_suffix ) = 0;
+        virtual void Visit( typename modifier::template m< struct PostfixSuffixVariable>::type & postfix_suffix ) = 0;
+        virtual void Visit( typename modifier::template m< struct ConstructorExpression>::type & expression ) = 0;
+        virtual void Visit( typename modifier::template m< struct ConditionalExpression>::type & expression ) = 0;
+        virtual void Visit( typename modifier::template m< struct LValueExpression>::type & expression ) = 0;
+        virtual void Visit( typename modifier::template m< struct PreModifyExpression>::type & expression ) = 0;
+        virtual void Visit( typename modifier::template m< struct PostModifyExpression>::type & expression ) = 0;
+        virtual void Visit( typename modifier::template m< struct CastExpression>::type & expression ) = 0;
+        virtual void Visit( typename modifier::template m< struct AssignmentExpression>::type & expression ) = 0;
+        virtual void Visit( typename modifier::template m< struct PostfixExpression>::type & expression ) = 0;
 
         // Statements
 
-        virtual void Visit( typename modifier::template m<ReturnStatement>::type & statement ) = 0;
-        virtual void Visit( typename modifier::template m<BreakStatement>::type & statement ) = 0;
-        virtual void Visit( typename modifier::template m<ContinueStatement>::type & statement ) = 0;
-        virtual void Visit( typename modifier::template m<DiscardStatement>::type & statement ) = 0;
-        virtual void Visit( typename modifier::template m<EmptyStatement>::type & statement ) = 0;
-        virtual void Visit( typename modifier::template m<ExpressionStatement>::type & statement ) = 0;
-        virtual void Visit( typename modifier::template m<IfStatement>::type & statement ) = 0;
-        virtual void Visit( typename modifier::template m<WhileStatement>::type & statement ) = 0;
-        virtual void Visit( typename modifier::template m<DoWhileStatement>::type & statement ) = 0;
-        virtual void Visit( typename modifier::template m<BlockStatement>::type & statement ) = 0;
-        virtual void Visit( typename modifier::template m<AssignmentStatement>::type & statement ) = 0;
-        virtual void Visit( typename modifier::template m<VariableDeclarationStatement>::type & statement ) = 0;
+        virtual void Visit( typename modifier::template m< struct ReturnStatement>::type & statement ) = 0;
+        virtual void Visit( typename modifier::template m< struct BreakStatement>::type & statement ) = 0;
+        virtual void Visit( typename modifier::template m< struct ContinueStatement>::type & statement ) = 0;
+        virtual void Visit( typename modifier::template m< struct DiscardStatement>::type & statement ) = 0;
+        virtual void Visit( typename modifier::template m< struct EmptyStatement>::type & statement ) = 0;
+        virtual void Visit( typename modifier::template m< struct ExpressionStatement>::type & statement ) = 0;
+        virtual void Visit( typename modifier::template m< struct IfStatement>::type & statement ) = 0;
+        virtual void Visit( typename modifier::template m< struct WhileStatement>::type & statement ) = 0;
+        virtual void Visit( typename modifier::template m< struct DoWhileStatement>::type & statement ) = 0;
+        virtual void Visit( typename modifier::template m< struct BlockStatement>::type & statement ) = 0;
+        virtual void Visit( typename modifier::template m< struct AssignmentStatement>::type & statement ) = 0;
+        virtual void Visit( typename modifier::template m< struct VariableDeclarationStatement>::type & statement ) = 0;
 
         template< class _Table_, typename _Separator_ >
         void VisitTable( ConstVisitor & visitor, _Table_ & table, _Separator_ & separator )
