ac92e96c4e9d107908acd119d43f1f1356091e34
Julien Hamaide <julien.hamaide@fishingcactus.com>
2015-03-11 16:47:11 +0100

    Change some signature preparing for emscripten

--- src/hlsl_parser/hlsl.cpp
+++ src/hlsl_parser/hlsl.cpp
@@ -4,10 +4,9 @@
 #include "hlsl_parser/HLSLParser.hpp"
 #include "ast/node.h"
 
-AST::TranslationUnit * HLSL::ParseHLSL( const char * filename )
+Base::ObjectRef<AST::TranslationUnit> HLSL::ParseHLSL( const string & filename )
 {
-
-    HLSLLexerTraits::InputStreamType input( (ANTLR_UINT8*)filename, ANTLR_ENC_8BIT );
+    HLSLLexerTraits::InputStreamType input( (ANTLR_UINT8*)filename.c_str(), ANTLR_ENC_8BIT );
     HLSLLexer lexer( &input );
     HLSLLexerTraits::TokenStreamType token_stream( ANTLR_SIZE_HINT, lexer.get_tokSource() );
     HLSLParser parser( &token_stream );
