d42ecc821928183627b5d5a243bf76136a70d4bd
Michael Delva <michael.delva@fishingcactus.com>
2015-05-07 09:52:00 +0200

    Added test for missing f for floats

--- test/src/ast/legalizer/legalizer_test.cpp
+++ test/src/ast/legalizer/legalizer_test.cpp
@@ -5,7 +5,7 @@
 
 TEST_CASE( "Legalizer", "" )
 {
-    SECTION( "Integers assigned to float are converted" )
+    SECTION( "Float declaration with integer initial value is converted" )
     {
         AST::VariableDeclaration
             node;
@@ -21,12 +21,30 @@ TEST_CASE( "Legalizer", "" )
 
         node.m_BodyTable.back()->m_InitialValue->AddExpression( literal_expression );
 
-        /*node.m_InitialValue = new AST::InitialValue();
-        node.m_InitialValue->m_Vector = true;
-        node.m_InitialValue->AddExpression( new AST::LiteralExpression( AST::LiteralExpression::Float, "0.0f" ) );
-        node.m_InitialValue->AddExpression( new AST::LiteralExpression( AST::LiteralExpression::Float, "0.0f" ) );
-        node.m_InitialValue->AddExpression( new AST::LiteralExpression( AST::LiteralExpression::Float, "1.0f" ) );
-        node.m_InitialValue->AddExpression( new AST::LiteralExpression( AST::LiteralExpression::Float, "0.0f" ) );*/
+        AST::Legalizer
+            legalizer;
+
+        node.Visit( legalizer );
+
+        CHECK( literal_expression->m_Type == AST::LiteralExpression::Float );
+        CHECK( literal_expression->m_Value == "123.0f" );
+    }
+
+    SECTION( "Float declaration with missing f is converted" )
+    {
+        AST::VariableDeclaration
+            node;
+
+        node.SetType( new AST::Type( "float" ) );
+        node.AddBody( new AST::VariableDeclarationBody( "intensity" ) );
+        node.m_BodyTable.back()->m_InitialValue = new AST::InitialValue();
+
+        AST::LiteralExpression
+            * literal_expression;
+
+        literal_expression = new AST::LiteralExpression( AST::LiteralExpression::Float, "123.0" );
+
+        node.m_BodyTable.back()->m_InitialValue->AddExpression( literal_expression );
 
         AST::Legalizer
             legalizer;
