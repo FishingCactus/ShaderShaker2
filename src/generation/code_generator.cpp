00141e04a0c69a47aa11a345c7cf202fb28a0154
Pieter Vantorre <pieter.vantorre@fishingcactus.com>
2016-11-25 09:13:19 +0100

    Fixed unittest

--- src/generation/code_generator.cpp
+++ src/generation/code_generator.cpp
@@ -375,13 +375,11 @@ namespace Generation
 
         if( !graph )
         {
-            m_ErrorHandler->ReportError( "Unable to generate graph.", "" );
             return;
         }
 
         if( !ValidatesGraph( *graph ) )
         {
-            m_ErrorHandler->ReportError( "Generate graph is invalid.", "" );
             return;
         }
 
@@ -389,7 +387,6 @@ namespace Generation
 
         if( !function )
         {
-            m_ErrorHandler->ReportError( "Unable to generate code from graph.", "" );
             return;
         }
 
